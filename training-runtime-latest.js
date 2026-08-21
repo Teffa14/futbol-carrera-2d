fb02e6e3cc9bd11d76fff9df8fce210ec0461891
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-21 13:35:30 +1000

    Publish training scenarios v7

--- training-runtime-latest.js
+++ training-runtime-latest.js
@@ -4,7 +4,7 @@ import {TrainingMatchEngine,TRAINING_MATCH_ENGINE_VERSION} from './training-matc
 export const TRAINING_RUNTIME_VERSIONS=Object.freeze({
   matchEngine:TRAINING_MATCH_ENGINE_VERSION,
   framework:2,
-  authoritativeScenarios:6,
+  authoritativeScenarios:7,
   liveUi:5,
 });
 
