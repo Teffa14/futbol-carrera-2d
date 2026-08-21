4ba03d8ece9f8ce694beeb2eb7b07d290d2f2d6a
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-21 13:26:08 +1000

    chore: expose training engine v4 runtime

--- training-runtime-latest.js
+++ training-runtime-latest.js
@@ -4,7 +4,7 @@ import {TrainingMatchEngine,TRAINING_MATCH_ENGINE_VERSION} from './training-matc
 export const TRAINING_RUNTIME_VERSIONS=Object.freeze({
   matchEngine:TRAINING_MATCH_ENGINE_VERSION,
   framework:2,
-  authoritativeScenarios:3,
+  authoritativeScenarios:4,
   liveUi:5,
 });
 
